7ac31d889b9905e19269b7b71069e3ed11e2ed54
Kanwara <fonfonvfc@gmail.com>
2017-02-13 15:07:30 +0700

    add

---
+++ Desktop/GitHub.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kanwara
+ */
+public class GitHub {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("TestGITHUB");
+    }
+    
+}
